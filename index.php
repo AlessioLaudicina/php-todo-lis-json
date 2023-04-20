767868834e34d96bbee8f03bd15b91ff7e83960e
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-04-20 16:51:17 +0200

    aggiunta stile alla lista

--- index.php
+++ index.php
@@ -7,18 +7,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
-    <title>Document</title>
+    <title>php-todo-list</title>
     
 </head>
 <body>
 <div id="app">
-    <div class="container">
+    <h1 class="text-center text-white py-4" >To Do List</h1>
+    <div class="container d-flex justify-content-center align-items-center">
+        <div>
         <ul class="list-group" >
             <li v-for="todo in todoList" class="list-group-item" >{{todo}}</li>
         </ul>
 
-        <input v-model="todoItem" type="text">
-        <button class="btn" @click="addTodo" >Aggiungi to Do</button>
+        
+
+        <input v-model="todoItem" type="text" class="p-1 mt-2 rounded">
+        <button class="btn btn-secondary mx-2" @click="addTodo" >Aggiungi</button>
+
+        </div>
+        
 
     </div>
 
